ba99d2d727d8df93cce80dcf5a5e81ab4abc3290
Hannes Rosseel <hannes.rosseel@esat.kuleuven.be>
2022-06-21 13:56:02 +0200

    Added multiple changes to localization.py

--- acoustdsp/localization.py
+++ acoustdsp/localization.py
@@ -25,7 +25,8 @@ import itertools
 import warnings
 
 import numpy as np
-from numba import njit
+
+from acoustdsp import cc_model
 
 
 def gcc(sig: np.ndarray, refsig: np.ndarray,
@@ -57,7 +58,7 @@ def gcc(sig: np.ndarray, refsig: np.ndarray,
         raise ValueError("This function currently only supports Direct and "
                          "PHAT weighting.")
 
-    fft_len = 2 * max([sig.shape[0], refsig.shape[0]])
+    fft_len = sig.shape[0] + refsig.shape[0] - 1
 
     SIG = np.fft.rfft(sig, n=fft_len, axis=0)
     REFSIG = np.fft.rfft(refsig, n=fft_len, axis=0)
@@ -96,20 +97,26 @@ def cc_parabolic_interp(R: np.ndarray, tdoa_region: np.ndarray, tau: float,
     tau: float
         Improved time delay estimation in seconds.
     """
-    R = np.atleast_2d(R)
-    max_indices = np.argmax(R[tdoa_region, :], axis=0) + tdoa_region[0]
+    if len(R.shape) < 2:
+        R = np.atleast_2d(R).T
 
+    max_indices = np.argmax(R[tdoa_region, :], axis=0) + tdoa_region[0]
     # Retrieve the values around the maximum of R
     y = np.array([R[idx - 1: idx + 2, i] for i, idx in enumerate(max_indices)])
 
-    # Perform parabolic interpolation and return the improved tau value.
-    d1 = y[:, 1] - y[:, 0]
-    d2 = y[:, 2] - y[:, 0]
-    a = -d1 + d2 / 2
-    b = 2 * d1 - d2 / 2
-    vertices = -b / (2 * a)
-    # vertex - 1 is the sample-offset from maximum point of R
-    return tau + (vertices - 1) / fs
+    tau_imp = tau
+    for i, y_i in enumerate(y):
+        if len(y_i) == 3:
+            # Perform parabolic interpolation and return the improved tau
+            # value.
+            d1 = y_i[1] - y_i[0]
+            d2 = y_i[2] - y_i[0]
+            a = -d1 + d2 / 2
+            b = 2 * d1 - d2 / 2
+            vertices = -b / (2 * a)
+            # vertex - 1 is the sample-offset from maximum point of R
+            tau_imp[i] += (vertices - 1) / fs
+    return tau_imp
 
 
 def cc_gaussian_interp(R: np.ndarray, tdoa_region: np.ndarray, tau: float,
@@ -138,14 +145,17 @@ def cc_gaussian_interp(R: np.ndarray, tdoa_region: np.ndarray, tau: float,
     tau: float
         Improved time delay estimation in seconds.
     """
-    R = np.atleast_2d(R)
+    if len(R.shape) < 2:
+        R = np.atleast_2d(R).T
     max_indices = np.argmax(R[tdoa_region, :], axis=0) + tdoa_region[0]
 
     # Retrieve the values around the maximum of R. R needs to be positive for
     # indices around the maximum value. If this is not the case, take the
     # absolute value of the point (impacts fitting).
-    y = np.array([[R[val - 1, idx], R[val, idx], R[val + 1, idx]]
-                  for idx, val in enumerate(max_indices)])
+    y_ind = max_indices[:, np.newaxis] + np.arange(-1, 2)
+    y_ind = np.where(y_ind < 0, 0, y_ind)
+    y_ind = np.where(y_ind >= R.shape[0], R.shape[0] - 1, y_ind)
+    y = R[y_ind, np.arange(0, y_ind.shape[0]).reshape(-1, 1)]
 
     if (y < 0).any():
         warnings.warn("Gaussian interpolation encountered negative R values. "
@@ -160,15 +170,14 @@ def cc_gaussian_interp(R: np.ndarray, tdoa_region: np.ndarray, tau: float,
     return tau + c / fs
 
 
-@njit
-def cc_sinc_interp(R: np.ndarray, tau: float, interp_mul: int, fs: int,
+def cc_sinc_interp(r: np.ndarray, tau: float, interp_mul: int, fs: int,
                    half_width: float = 0.002):
     """
     Fit a critically sampled sinc function to the maximum value of the
     cross-correlation function. Returns the improved time-delay found by the
     fitting.
 
-    Parameters
+        Parameters
     ----------
     R: np.ndarray
         Input cross-correlation signal. R has a size of (2N-1) x S. Where
@@ -196,31 +205,168 @@ def cc_sinc_interp(R: np.ndarray, tau: float, interp_mul: int, fs: int,
         raise ValueError("Interpolation multiplier has to be a strictly"
                          " positive integer.")
 
-    R = np.atleast_2d(R)
-    max_ind = np.argmax(R, axis=0).astype(np.int32)
+    if len(r.shape) == 1:
+        r = np.atleast_2d(r).T
+    max_ind = np.asarray(np.argmax(r, axis=0))
 
     fs_res = fs * interp_mul
     max_ind_res = max_ind * interp_mul
 
-    # Search 10 samples around the direct path component
-    n_margin_res = int(5 * interp_mul)
-    search_area = (max_ind_res + np.arange(-n_margin_res, n_margin_res + 1
+    # Search 1 sample around the direct path component
+    search_area = (max_ind_res + np.arange(-interp_mul, interp_mul + 1
                                            ).reshape((-1, 1))) / fs_res
     n_half_width = int(half_width * fs)
     window = max_ind + np.arange(-n_half_width, n_half_width
                                  + 1).reshape((-1, 1))
-    window = np.where(window < R.shape[0], window, R.shape[0] - 1)
+    window = np.where(window < r.shape[0], window, r.shape[0] - 1)
 
     cost_vector = np.zeros(search_area.shape)
-    amplitudes = [R[i_max, i] for i, i_max in enumerate(max_ind)]
+    amplitudes = [r[i_max, i] for i, i_max in enumerate(max_ind)]
 
     t = window / fs
-    for i, r in enumerate(R.T):
+    for i, r_i in enumerate(r.T):
         for j, t_0 in enumerate(search_area[:, i]):
             cost_vector[j, i] = np.sum(np.square(np.sinc(fs * (t[:, i] - t_0))
-                                       - r[window[:, i]] / amplitudes[i]))
+                                       - r_i[window[:, i]] / amplitudes[i]))
     minima = np.argmin(cost_vector, axis=0)
-    return (minima - n_margin_res) / fs_res + tau
+    return (minima - interp_mul) / fs_res + tau
+
+
+def cc_sinc_interp2(r: np.ndarray, tau: float, interp_mul: int, fs: int,
+                    half_width: float = None):
+    """
+    Fit a critically sampled sinc function to the maximum value of the
+    cross-correlation function. Returns the improved time-delay found by the
+    fitting.
+
+        Parameters
+    ----------
+    R: np.ndarray
+        Input cross-correlation signal. R has a size of (2N-1) x S. Where
+        S is the number of cross-correlations.
+    tau: float
+        Estimated time delay value in seconds which maximizes the
+        cross-correlation function `R`.
+    interp_mul: int
+        Interpolation factor equal to `T / T_i`. Where `T` is the sampling
+        period of the original sampled signal. `T_i` is the interpolation
+        sampling period.
+    fs: int, optional
+        Signal sampling rate in Hz, specified as a real-valued scalar.
+        Defaults to 1.
+    half_width: float
+        interpolation half width of the sinc fitting. Specifies the maximum
+        time-delay to fit the sinc funtion around the maximum of the
+        cross-correlation function.
+    Returns
+    -------
+    tau: float
+        Improved time delay estimation in seconds.
+    """
+    if(interp_mul <= 0):
+        raise ValueError("Interpolation multiplier has to be a strictly"
+                         " positive integer.")
+    if half_width is None:
+        half_width = (r.shape[0] // 2) / fs
+
+    if len(r.shape) == 1:
+        r = np.atleast_2d(r).T
+    max_ind = np.asarray(np.argmax(r, axis=0))
+
+    # Search 1 sample around the direct path components
+    search_area = (max_ind + np.arange(-interp_mul, interp_mul + 1
+                                       ).reshape((-1, 1)) / interp_mul) / fs
+    n_half_width = int(half_width * fs)
+    window = max_ind + np.arange(-n_half_width, n_half_width
+                                 + 1).reshape((-1, 1))
+
+    # Make sure that window is not outside of r
+    window = np.where(window < r.shape[0], window, r.shape[0] - 1)
+
+    cost_vector = np.zeros(search_area.shape)
+    amplitudes = [r[i_max, i] for i, i_max in enumerate(max_ind)]
+
+    t = window / fs
+    for i, r_i in enumerate(r.T):
+        for j, t_0 in enumerate(search_area[:, i]):
+            sinc = np.sinc(fs * (t[:, i] - t_0))
+            cost_vector[j, i] = np.sum(np.square(sinc / np.max(sinc)
+                                       - r_i[window[:, i]] / amplitudes[i]))
+    minima = np.argmin(cost_vector, axis=0)
+    return search_area[minima, np.arange(0, minima.shape[0])] \
+        - (max_ind / fs) + tau
+
+
+def cc_whittaker_shannon_interp(r: np.ndarray, tau_est: float,
+                                num_points: int = None,
+                                interp_factor: int = 100, fs: int = 1):
+    """
+    Function under test
+    """
+    if num_points is None:
+        num_points = r.shape[0]
+    if len(r.shape) == 1:
+        r = np.atleast_2d(r).T
+
+    argmax = np.argmax(r, axis=0).astype(np.int32)
+    windows = argmax + np.arange(-num_points // 2, num_points // 2
+                                 ).reshape(-1, 1)
+
+    n_interp = argmax + (np.arange(-interp_factor // 2, interp_factor // 2 + 1)
+                         / interp_factor).reshape(-1, 1)
+    t_interp = (n_interp - argmax) / fs
+
+    reconst = np.zeros((interp_factor + 1, r.shape[1]))
+
+    for idx, window in enumerate(windows.T):
+        window -= np.min(window) if np.min(window) < 0 else 0
+        window -= (np.max(window) - (r.shape[0] - 1)
+                   if np.max(window) >= r.shape[0] else 0)
+
+        reconst[:, idx] = np.sum(r[window, idx, np.newaxis]
+                                 * np.sinc(n_interp[:, idx]
+                                 - window.reshape(-1, 1)), axis=0)
+
+    max_vals = np.argmax(reconst, axis=0)
+    tau_delta = np.array([t_interp[val, idx] for idx, val
+                          in enumerate(max_vals)]).T
+    return tau_est + tau_delta
+
+
+def cc_windowed_sinc_interp(r: np.ndarray, t0_est: float, t1_est: float,
+                            interp_mul: int, fs: int):
+    if(interp_mul <= 0):
+        raise ValueError("Interpolation multiplier has to be a strictly"
+                         " positive integer.")
+
+    # Ensure `r` is a 2D Numpy array.
+    if len(r.shape) == 1:
+        r = np.atleast_2d(r).T
+
+    L = (r.shape[0] + 1) / (2 * fs)
+    t_corr = np.arange(-L * fs + 1, L * fs) / fs
+
+    fs_res = fs * interp_mul
+
+    # Search a single sample around tau_1 and tau_2
+    search_area_t0 = t0_est + np.arange(-interp_mul, interp_mul + 1
+                                        ).reshape(-1, 1) / (2 * fs_res)
+    search_area_t1 = t1_est + np.arange(-interp_mul, interp_mul + 1
+                                        ).reshape(-1, 1) / (2 * fs_res)
+
+    models = np.zeros((t_corr.shape[0], search_area_t0.shape[0],
+                       search_area_t1.shape[0]))
+    for t0_idx, t0 in enumerate(search_area_t0):
+        for t1_idx, t1 in enumerate(search_area_t1):
+            models[:, t0_idx, t1_idx] = cc_model(t_corr, fs * np.pi, t0, t1, L)
+
+    difference = models - r[:, :, np.newaxis]
+    cost_vector = np.sum(np.square(difference), axis=0)
+
+    minimum = np.min(cost_vector)
+    est0_idx, est1_idx = np.where(cost_vector == minimum)
+
+    return search_area_t0[est0_idx] - search_area_t1[est1_idx]
 
 
 def calculate_tdoa(rirs: np.ndarray, mic_pairs: np.ndarray, max_td: int,
@@ -352,7 +498,7 @@ def calculate_doa(tau_hat: np.ndarray, V: np.ndarray):
         The number of microphone pairs is: P = num_mics * (num_mics - 1) / 2.
         This vector has length (Px1)
     V: np.ndarray
-        The sensor vector matrix in 3D space. V is an (P x D)
+        The sensor difference matrix in 3D space. V is an (P x D)
         matrix, with P number of microphone pairs and D physical
         dimensions.
     Returns
@@ -366,6 +512,33 @@ def calculate_doa(tau_hat: np.ndarray, V: np.ndarray):
     return k_hat if np.sum(k_hat, axis=0) == 0 else (-k_hat
                                                      / np.linalg.norm(k_hat, 2,
                                                                       axis=0))
+
+
+def get_sensor_difference_matrix(mic_array):
+    """
+    Calculate the sensor difference matrix of a given microphone
+    array. This is equal to the difference of all possible
+    microphone pairs in the array.
+
+    Parameters
+    ----------
+    mic_array: np.ndarray
+        Microphone array carthesian coordinates (M x D), where M is
+        the number of microphones and D is the number of dimensions.
+    Returns
+    -------
+    V: np.ndarray
+        The sensor difference matrix in 3D space. V is an (P x D)
+        matrix, with P number of microphone pairs and D physical
+        dimensions.
+
+    """
+    # All possible microphone pairs P
+    mic_pairs = np.array(list(itertools.combinations(range(mic_array.shape[0]),
+                                                     2)))
+    # Define the sensor difference matrix in 3D space (P x D)
+    V = mic_array[mic_pairs[:, 0], :] - mic_array[mic_pairs[:, 1], :]
+    return V
 
 
 def get_propagation_time(mic_array: np.ndarray, fs: int = 1, c: float = 343.):
@@ -391,18 +564,9 @@ def get_propagation_time(mic_array: np.ndarray, fs: int = 1, c: float = 343.):
     max_td: int
         The maximum time in samples for a wavefront to propagate through
         the given microphone array configuration.
-    V: np.ndarray
-        The sensor vector matrix in 3D space. V is an (P x D)
-        matrix, with P number of microphone pairs and D physical
-        dimensions.
     """
-    # All possible microphone pairs P
-    mic_pairs = np.array(list(itertools.combinations(range(mic_array.shape[0]),
-                                                     2)))
-    # Define the sensor vector matrix in 3D space (P x D)
-    V = mic_array[mic_pairs[:, 0], :] - mic_array[mic_pairs[:, 1], :]
+    V = get_sensor_difference_matrix(mic_array)
     # Maximum distance and time delay
     max_distance = max(np.linalg.norm(V, 2, axis=1))
     # Maximum accepted time delay difference
-    max_td = int(np.ceil(max_distance / c * fs)) + 1
-    return (max_td, V)
+    return np.ceil(max_distance / c * fs).astype(int)
